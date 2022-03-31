8fce833e79c5218cdfc7c6d7a6db9151b2801859
NATHALIA APARECIDA RAMOS <21276387@CFP127ALUNO.LOCAL>
2022-03-31 16:50:29 -0300

    player vs player

--- app/src/main/java/com/example/projeto_jogo_da_velha/MainActivity.kt
+++ app/src/main/java/com/example/projeto_jogo_da_velha/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.projeto_jogo_da_velha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val Fácil = findViewById<Button>(R.id.facil)
+        val medio = findViewById<Button>(R.id.medio)
+        val dificil = findViewById<Button>(R.id.dificil)
+
+
     }
 }
--- app/src/main/java/com/example/projeto_jogo_da_velha/MainActivity2.kt
+++ app/src/main/java/com/example/projeto_jogo_da_velha/MainActivity2.kt
@@ -1,11 +1,27 @@
 package com.example.projeto_jogo_da_velha
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val playerVSplayer = findViewById<Button>(R.id.playerVSplayer)
+        val playerVScomp = findViewById<Button>(R.id.playerVScomp)
+
+        playerVSplayer.setOnClickListener{
+           val intent = Intent(this, playerVSplayer::class.java)
+            startActivity(intent)
+        }
+
+        playerVScomp.setOnClickListener({
+            val intent = Intent(this,playerVScomp::class.java )
+            startActivity(intent)
+        })
+
     }
 }
